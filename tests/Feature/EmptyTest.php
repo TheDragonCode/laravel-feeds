9db28a9b0ef8a265407e60b66420ed064831eeba
Andrey Helldar <helldar@dragon-code.pro>
2025-08-31 00:47:46 +0300

    Code duplication is removed

--- tests/Feature/EmptyTest.php
+++ tests/Feature/EmptyTest.php
@@ -2,18 +2,10 @@
 
 declare(strict_types=1);
 
-use DragonCode\LaravelFeed\Console\Commands\FeedGenerateCommand;
 use Workbench\App\Feeds\EmptyFeed;
-
-use function Pest\Laravel\artisan;
 
 test('export', function (bool $pretty) {
     setPrettyXml($pretty);
 
-    $feed = app()->make(EmptyFeed::class);
-
-    artisan(FeedGenerateCommand::class)->run();
-
-    expect($feed->path())->toBeReadableFile();
-    expect(file_get_contents($feed->path()))->toMatchSnapshot();
+    expectFeed(EmptyFeed::class);
 })->with('boolean');
--- tests/Feature/YandexTest.php
+++ tests/Feature/YandexTest.php
@@ -2,18 +2,10 @@
 
 declare(strict_types=1);
 
-use DragonCode\LaravelFeed\Console\Commands\FeedGenerateCommand;
 use Workbench\App\Feeds\YandexFeed;
-
-use function Pest\Laravel\artisan;
 
 test('export', function () {
     createProducts();
 
-    $feed = app()->make(YandexFeed::class);
-
-    artisan(FeedGenerateCommand::class)->run();
-
-    expect($feed->path())->toBeReadableFile();
-    expect(file_get_contents($feed->path()))->toMatchSnapshot();
+    expectFeed(YandexFeed::class);
 });
