26ec7697ba52193451fad259b6904a0c071c5a7e
andrey-helldar <10347617+andrey-helldar@users.noreply.github.com>
2025-08-31 19:11:50 +0000

    🦋 The code style has been fixed

---
+++ Pest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+pest()
+    ->printer()
+    ->compact();
+
+pest()
+    ->extend(TestCase::class)
+    ->use(RefreshDatabase::class)
+    ->in('Feature');
+
+pest()
+    ->extend(TestCase::class)
+    ->in('Unit');
