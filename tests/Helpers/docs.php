cd91290216882815b8375d008772f34d3f50074b
Andrey Helldar <helldar@dragon-code.pro>
2025-09-04 22:55:02 +0300

    Fixed path to workbench

--- tests/Helpers/docs.php
+++ tests/Helpers/docs.php
@@ -2,9 +2,11 @@
 
 declare(strict_types=1);
 
+use function Orchestra\Testbench\workbench_path;
+
 function copyFeedFileToDoc(string $source, string $target, bool $removeFilename = true): void
 {
-    $sourceFile = __DIR__ . '/../../workbench/app/Feeds/Docs/' . $source . '.php';
+    $sourceFile = workbench_path('app/Feeds/Docs/' . $source . '.php');
     $targetFile = __DIR__ . '/../../docs/snippets/' . $target;
 
     $content = file_get_contents($sourceFile);
